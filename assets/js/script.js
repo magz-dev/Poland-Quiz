a575dcfee71440f488f292405a14ccdbb7802f1f
magzdev <magdakloskowicz@gmail.com>
2023-09-17 19:35:19 +0000

    Add a runGame() function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,35 +1,4 @@
 
-function runGame() {
-
-}
-
-
-function setNextQuestion() {
-
-}
-
-
-
-function displayQuestion() {
-    if (currentQuestionIndex < questions.length) {
-        questionText.textContent = questions[currentQuestionIndex].question;
-        answerText.innerHTML = "";
-        questions[currentQuestionIndex].answer-btn.forEach((answer)=> {
-        button.textContent = answer;
-        button.addEventListener("click", checkAnswer);
-        })
-        }
-    }
-
-
-
-function checkAnswer(){
-
-}
-
-
-
-
 const questions = [
     {
         question: 'What is a capital city of Poland?',
@@ -42,4 +11,38 @@ const questions = [
 
 let currentQuestionIndex = 0;
 const questionText = document.getElementById("question");
-const answerText = document.getElementById("answer-btn");
+const answerButtons = document.getElementById("answers");
+const nextButton = document.getElementById("next-btn");
+
+function runGame() {
+currentQuestionIndex = 0;
+nextButton.innerHTML = "Next";
+displayQuestion();
+}
+
+
+function displayQuestion() {
+    setNextQuestion();
+    if (currentQuestionIndex < questions.length) {
+        questionText.textContent = questions[currentQuestionIndex].question;
+        answerButtons.innerHTML = "";
+        questions[currentQuestionIndex].answers.forEach((answer)=> {
+        const button = document.createElement("button");
+        button.textContent = answer.text;
+        button.addEventListener("click", checkAnswer);
+        optionsElement.appendChild(button);
+    })}
+    }
+
+
+    function setNextQuestion() {
+     
+    }
+
+function checkAnswer(){
+
+}
+
+
+
+
