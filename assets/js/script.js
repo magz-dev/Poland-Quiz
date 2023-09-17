83484d67f9069abe66cb4c1ba0a40bdbb387b6b0
magzdev <magdakloskowicz@gmail.com>
2023-09-17 18:53:17 +0000

    Add a displayQuestion() function

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,8 +11,16 @@ function setNextQuestion() {
 
 
 function displayQuestion() {
+    if (currentQuestionIndex < questions.length) {
+        questionText.textContent = questions[currentQuestionIndex].question;
+        answerText.innerHTML = "";
+        questions[currentQuestionIndex].answer-btn.forEach((answer)=> {
+        button.textContent = answer;
+        button.addEventListener("click", checkAnswer);
+        })
+        }
+    }
 
-}
 
 
 function checkAnswer(){
@@ -26,11 +34,12 @@ const questions = [
     {
         question: 'What is a capital city of Poland?',
         answers: [
-            {text: 'Warszawa', correct: true},
-            {text: 'Krakow', correct: false},
 
         ]
     }
 
 ]
 
+let currentQuestionIndex = 0;
+const questionText = document.getElementById("question");
+const answerText = document.getElementById("answer-btn");
